331c0318e4ad58a3f46182dec531ff4013eb1449
mleroy <mleroy@student.42.fr>
2018-01-13 19:26:30 +0100

    [WIP]master

--- srcs/main.c
+++ srcs/main.c
@@ -6,14 +6,22 @@
 /*   By: mleroy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/13 14:45:05 by mleroy            #+#    #+#             */
-/*   Updated: 2018/01/13 15:03:50 by mleroy           ###   ########.fr       */
+/*   Updated: 2018/01/13 19:06:03 by mleroy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft/libft.h>
+#include <stdio.h>
+#include "../libft/libft.h"
+#include "../includes/libft_malloc.h"
 
 int		main(void)
 {
-	ft_putstr("Hello World\n");
+	char	*ptr;
+
+	printf("getpagesize: %d\n", getpagesize());
+	printf("--- Malloc ---\n");
+	printf("[MALLOC]returns: %p\n", malloc(12));
+	
+	ptr = (char *)malloc(12);
 	return (0);
 }
--- srcs/malloc.c
+++ srcs/malloc.c
@@ -1,19 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_malloc.h                                     :+:      :+:    :+:   */
+/*   malloc.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mleroy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/13 15:08:09 by mleroy            #+#    #+#             */
-/*   Updated: 2018/01/13 18:03:36 by mleroy           ###   ########.fr       */
+/*   Created: 2018/01/13 17:58:44 by mleroy            #+#    #+#             */
+/*   Updated: 2018/01/13 18:03:39 by mleroy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_MALLOC_H
-# define LIBFT_MALLOC_H
-# include <unistd.h>
-	void	free(void *ptr);
-	void	*malloc(size_t size);
-	void	*realloc(void *ptr, size_t size);
-#endif
+#include "../includes/libft_malloc.h"
+
+void	*malloc(size_t size)
+{
+	void	*addr;
+
+	return (addr);
+}
